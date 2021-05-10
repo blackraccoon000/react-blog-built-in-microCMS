e3d8d352810b92a6a92b982ccac42f26b47d631a
Yutaka Fujii <yutakaf@playwell.site>
2021-05-10 19:20:17 +0900

    index.html

--- webpack/webpack.common.js
+++ webpack/webpack.common.js
@@ -1,5 +1,9 @@
 const path = require('path');
 const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+// console.log(path.resolve(__dirname, '../src/template/index.html'));
+// console.log(path.resolve(__dirname, '../public/dist'));
 
 module.exports = {
   entry: './src/app.js',
@@ -41,5 +45,16 @@ module.exports = {
       },
     ],
   },
-  plugins: [],
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'My Template X',
+      //生成する HTML ファイル
+      filename: path.resolve(__dirname, '../public/index.html'),
+      hash: true,
+      // テンプレートで使用するファイルのパスを指定
+      template: path.resolve(__dirname, '../src/template/index.html'),
+      // テンプレートで使用する変数 h1 を設定
+      h1: 'Heading Title H1',
+    }),
+  ],
 };
