5860e44386691fd828168912c232901670035b10
Yutaka Fujii <yutakaf@playwell.site>
2021-05-18 22:57:10 +0900

    express + loadingCSS

---
+++ src/stories/L1_Atoms/Loading.stories.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Loading from '../../components/L1_Atoms/Loading';
+
+export default {
+  title: 'L1_Atoms/Loading',
+  component: Loading,
+};
+
+const Template = (args) => <Loading {...args} />;
+
+export const Default = Template.bind({});
+Default.args = {};
