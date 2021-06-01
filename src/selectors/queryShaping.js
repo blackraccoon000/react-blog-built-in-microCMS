8e3c9f8bee36595e8e6a4980c21b2c576f93790a
Yutaka Fujii <yutakaf@playwell.site>
2021-06-01 17:29:56 +0900

    netlify up

--- src/selectors/queryShaping.js
+++ src/selectors/queryShaping.js
@@ -3,7 +3,7 @@ const keywordFields = 'id,title,keyword,createdAt,updatedAt';
 
 const acqId = process.env.ACQID;
 // const url = "http://0.0.0.0:3000/acq"
-const url = 'http://0.0.0.0:3000/acqhttp://blog-api.playwell.site/';
+const url = 'http://blog-api.playwell.site/acq';
 const str = 'https://playwell.microcms.io/api/v1/blog';
 import store from '../store/store';
 
