7aeb20c35b9a6b8d55941fc38317cdcfcce61e4c
Yutaka Fujii <yutakaf@playwell.site>
2021-06-01 17:18:27 +0900

    netlify setup

--- src/selectors/queryShaping.js
+++ src/selectors/queryShaping.js
@@ -2,13 +2,15 @@ const fields = 'id,title,keyword,thumbnail,createdAt,updatedAt,body';
 const keywordFields = 'id,title,keyword,createdAt,updatedAt';
 
 const acqId = process.env.ACQID;
+// const url = "http://0.0.0.0:3000/acq"
+const url = 'http://0.0.0.0:3000/acqhttp://blog-api.playwell.site/';
 const str = 'https://playwell.microcms.io/api/v1/blog';
 import store from '../store/store';
 
 const queryShaping = (command, id) => {
   switch (command) {
     case 'start':
-      return `http://0.0.0.0:3000/acq/${acqId}`;
+      return `${url}/${acqId}`;
     case 'acquired':
       return `${str}?ids=${id}&fields=body`;
     default:
